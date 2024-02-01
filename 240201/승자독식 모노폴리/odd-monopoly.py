1fb527a9da5562a3523dd00c6d3752a87a27b412
Yesong Lee <redjean71@naver.com>
2024-02-01 17:10:28 +0900

    Title: 승자독식 모노폴리, Time: 336ms, Memory: 27MB, Status: Passed - CodeTree

--- 240201/승자독식 모노폴리/odd-monopoly.py
+++ 240201/승자독식 모노폴리/odd-monopoly.py
@@ -33,7 +33,7 @@ def move():
             isDead[player_num] = Dead
             continue
         grid[x][y] = (player_num+1)
-        time[x][y] = (K)
+        time[x][y] = (K+1)
         current[player_num] = next[player_num]
     next = [[] for _ in range(M)]
 
@@ -79,7 +79,7 @@ time = [[0 for i in range(N)] for j in range(N)]
 for i in range(N):
     for j in range(N):
         if grid[i][j] != 0:
-            time[i][j] = (K)
+            time[i][j] = (K+1)
             player_num = grid[i][j]
             current[player_num-1] = [i,j,first_d[player_num-1]-1]
 Dead,Alive = 0,1
