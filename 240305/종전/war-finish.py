d6b3ae34df271a08465b9e72cf4971ce0f841ac6
Yesong Lee <redjean71@naver.com>
2024-03-05 15:35:41 +0900

    Title: 종전, Time: 851ms, Memory: 29MB, Status: Passed - CodeTree

--- 240305/종전/war-finish.py
+++ 240305/종전/war-finish.py
@@ -53,21 +53,41 @@ def coor_in_rec(ver_coor,width,length):
     rec_coor.append(border_coor[-1])
     return rec_coor
 
-def get_tribe_num(ver_coor,rec_coor):
+def get_tribe_num(ver_coor,border_coor):
     tribe = [0,0,0,0,0]
     one,two,three,four,_ = ver_coor
-    for x in range(N):
+    sx,ex = border_coor[0][0],border_coor[-1][0]
+    for tmpx in range(sx+1):
         for y in range(N):
-            if (x,y) in rec_coor:
+            if (tmpx,y)==three:
+                tribe[0] += people[tmpx][y]
+            elif y<=three[1]:
+                tribe[1] += people[tmpx][y]
+            else:
+                tribe[2] += people[tmpx][y]
+    idx1, idx2 = 1, 2
+    for b in range(1,len(border_coor)-1,2):
+        x,sy = border_coor[b]
+        _,ey = border_coor[b+1]
+        if x==four[0]:
+            idx1 += 2
+        if x==(two[0]+1):
+            idx2 += 2
+        for y in range(N):
+            if sy<=y<=ey:
                 tribe[0] += people[x][y]
-            elif x<four[0] and y<=three[1]:
-                tribe[1] += people[x][y]
-            elif x<=two[0] and y>three[1]:
-                tribe[2] += people[x][y]
-            elif x>=four[0] and y<one[1]:
-                tribe[3] += people[x][y]
-            elif x>two[0] and y>=one[1]:
-                tribe[4] += people[x][y]
+            elif y<sy:
+                tribe[idx1] += people[x][y]
+            else:
+                tribe[idx2] += people[x][y]
+    for tmpx in range(ex,N):
+        for y in range(N):
+            if (tmpx,y)==one:
+                tribe[0] += people[tmpx][y]
+            elif y<one[1]:
+                tribe[3] += people[tmpx][y]
+            else:
+                tribe[4] += people[tmpx][y]
     return tribe
 
 ####### MAIN ########
@@ -82,9 +102,9 @@ for width in range(1,N-1): # 사각형 가로
             for j in range(N): # 좌표 y
                 ver_coor = can_make_rectangular(i,j,width,length)
                 if ver_coor != False:
-                    rec_coor = coor_in_rec(ver_coor,width,length)
-                    tribe = get_tribe_num(ver_coor,rec_coor)
-                    #print(rec_coor,tribe,max(tribe)-min(tribe))
+                    border_coor = coor_in_border(ver_coor,width,length)
+                    tribe = get_tribe_num(ver_coor,border_coor)
+                    #print(ver_coor,tribe,max(tribe)-min(tribe))
                     if max(tribe)-min(tribe) < answer:
                         answer = max(tribe)-min(tribe)
 print(answer)
